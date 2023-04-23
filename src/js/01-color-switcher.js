7195b9f748782778a8b9c529fb45b8c6a0bda855
MariaOstapovych <m.ost.olgv@gmail.com>
2023-04-23 19:22:45 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-console.log("object");
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timer = null;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+buttonStart.addEventListener("click", () => {
+  timer = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      bodyEl.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+
+buttonStop.addEventListener("click", () => {
+  clearInterval(timer);
+});
